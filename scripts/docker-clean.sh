f0ed5ea990e0a4f191ddf878e64e851e10e8964d
MohamedHamou <mohamed.hamou@gisaia.com>
2019-07-22 14:36:53 +0200

    Start the stack with docker

---
+++ scripts/docker-clean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+function clean_exit {
+    ARG=$?
+    exit $ARG
+}
+trap clean_exit EXIT
+
+echo "===> stop arlas-tagger stack"
+docker-compose -f docker-compose-tagger.yml -f docker-compose-arlas-server.yml -f docker-compose-kafka.yml -f docker-compose-elasticsearch.yml --project-name arlas down -v
