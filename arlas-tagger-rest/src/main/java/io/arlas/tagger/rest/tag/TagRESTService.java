76f45b8271e431891aa96a9a9026324ce9f8a593
alainbodiguel <alain.bodiguel@gisaia.com>
2022-05-24 22:54:50 +0200

    development version 22.0.2-SNAPSHOT

--- arlas-tagger-rest/src/main/java/io/arlas/tagger/rest/tag/TagRESTService.java
+++ arlas-tagger-rest/src/main/java/io/arlas/tagger/rest/tag/TagRESTService.java
@@ -45,7 +45,7 @@ import java.util.Optional;
                 title = "ARLAS Tagger API",
                 description = "(Un)Tag fields of ARLAS collections",
                 license = @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0.html"),
-                version = "22.0.1"),
+                version = "API_VERSION"),
         schemes = { SwaggerDefinition.Scheme.HTTP, SwaggerDefinition.Scheme.HTTPS })
 
 public class TagRESTService {
