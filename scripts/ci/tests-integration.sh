e32005a95bf7122cb1178d4fc1f3587c622972ef
alainbodiguel <alain.bodiguel@gisaia.com>
2022-11-21 14:45:14 +0100

    feat: remove Zipkin

--- scripts/ci/tests-integration.sh
+++ scripts/ci/tests-integration.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -o errexit -o pipefail
 
-export ELASTIC_VERSION="7.17.4"
-export ARLAS_VERSION="22.0.1"
+export ELASTIC_VERSION="7.17.6"
+export ARLAS_VERSION="23.0.5"
 
 function clean_exit {
   ARG=$?
